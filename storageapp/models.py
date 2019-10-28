076c5391a14d40b7f82f8db7d602a36bb3cfbdde
evlx87 <evilnyagaming@yandex.ru>
2019-10-28 22:20:11 +0300

    add storageapp, model Sign

---
+++ storageapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Sign(models.Model):
+    normal_form = models.CharField(verbose_name='слово в нормальной форме', max_length=128)
+    url = models.ImageField(upload_to='normal_form_img')
+    other = models.CharField(verbose_name='еще какие-то данные', max_length=256)
