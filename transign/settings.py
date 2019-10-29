915bed01cdd1172db110d17b79d519401cb4bd38
evlx87 <evilnyagaming@yandex.ru>
2019-10-29 22:10:43 +0300

    modify views and settings

--- transign/settings.py
+++ transign/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import os
 from json import loads
 from os import path
 
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
