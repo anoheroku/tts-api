572a51cd499e94a8e1c013d2dd1d59800aa50e2a
evlx87 <evilnyagaming@yandex.ru>
2019-10-29 09:56:59 +0300

    modify Storageapp, models,views, post_json. DB

--- storageapp/admin.py
+++ storageapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from storageapp.models import Sign
 
-# Register your models here.
+admin.site.register(Sign)
--- storageapp/models.py
+++ storageapp/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 class Sign(models.Model):
     normal_form = models.CharField(verbose_name='слово в нормальной форме', max_length=128)
-    url = models.ImageField(upload_to='normal_form_img')
+    image = models.ImageField(upload_to='normal_form_img')
     other = models.CharField(verbose_name='еще какие-то данные', max_length=256)
+
+    def __str__(self):
+        return self.normal_form
